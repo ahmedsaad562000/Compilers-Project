660aacc3da8dfc9f7cad3e529451d37dd7eb82e0
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2024-05-16 20:37:43 +0300

    GUI added

---
+++ project.py
@@ -0,0 +1,96 @@
+from PyQt5 import QtWidgets,QtGui,QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QTextEdit, QVBoxLayout, QWidget,QTabWidget
+import sys
+import subprocess
+
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found."
+
+def compile_button_clicked(code_editor, quadraples_editor, semantic_error_editor, Symbol_Table_code_editor, generate_button):
+    generate_button.setText("Compiling...")
+    generate_button.setStyleSheet(" background-color: #FF0000; color: white; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 20px; margin: 4px 2px; cursor: pointer; width: 2px;")
+    QtWidgets.QApplication.processEvents()
+    #generate_button.setEnabled(False)
+    generate_file(code_editor.toPlainText(), quadraples_editor, semantic_error_editor, Symbol_Table_code_editor)
+    generate_button.setText("Compile Code")
+    generate_button.setStyleSheet(" background-color: #4CAF50; color: white; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 20px; margin: 4px 2px; cursor: pointer; width: 2px;")
+    QtWidgets.QApplication.processEvents()
+    #generate_button.setEnabled(True)
+
+
+def generate_file(code,quadraples_editor, semantic_error_editor, Symbol_Table_code_editor):
+    with open("test.txt", "w") as file:
+        file.write(code)
+    subprocess.run(["make", "-f", "Makefile"])
+    quadraples_editor.setPlainText(read_file("quadruple.txt"))
+    semantic_error_editor.setPlainText(read_file("semantic-error.txt"))
+    Symbol_Table_code_editor.setPlainText(read_file("symbol-tables.txt"))
+    
+
+def compile_code():
+    subprocess.run(["make", "-f", "Makefile"])
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setWindowTitle("Compiler")
+    font = QtGui.QFont()
+    font.setPointSize(12)
+    app.setFont(font)
+    
+    
+    central_widget = QWidget()
+    win.setCentralWidget(central_widget)
+    layout = QVBoxLayout(central_widget)
+
+
+    label = QLabel("Enter your code below:")
+    layout.addWidget(label)
+
+
+    code_editor = QTextEdit()
+    layout.addWidget(code_editor)
+    
+    
+    tab_widget = QTabWidget()
+
+
+    quadraples_editor = QTextEdit()
+    semantic_error_editor = QTextEdit()
+    Symbol_Table_code_editor = QTextEdit()
+    
+    
+    tab_widget.addTab(semantic_error_editor, "Semantic Errors")
+    tab_widget.addTab(Symbol_Table_code_editor, "Symbol Table")
+    tab_widget.addTab(quadraples_editor, "Quadraples")
+    for index in range(tab_widget.count()):
+        tab_widget.tabBar().setCursor(QtCore.Qt.PointingHandCursor)
+    
+    quadraples_editor.setPlainText("")
+    semantic_error_editor.setPlainText("")
+    Symbol_Table_code_editor.setPlainText("")    
+    layout.addWidget(tab_widget)
+    
+    
+    
+
+    generate_button = QPushButton("Compile Code")
+    generate_button.setCursor(QtCore.Qt.PointingHandCursor)
+    generate_button.setStyleSheet(" background-color: #4CAF50; color: white; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 20px; margin: 4px 2px; cursor: pointer; width: 2px;")
+    generate_button.setFixedWidth(200)
+    layout.addWidget(generate_button)
+    generate_button.clicked.connect(lambda: compile_button_clicked(code_editor, quadraples_editor, semantic_error_editor, Symbol_Table_code_editor, generate_button))
+    
+    
+
+    win.showMaximized()
+
+    sys.exit(app.exec_())
+
+window()
